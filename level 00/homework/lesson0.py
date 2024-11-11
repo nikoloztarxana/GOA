2c8057e337d6157edd2bfbfee6125b78d70359be
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2024-11-11 07:42:34 -0800

    first commit

---
+++ level 00/homework/lesson0.py
@@ -0,0 +1,80 @@
+print("nikoloztarxnishvili" * 100)
+
+# #concatination - კონკატინაცია არის ორი ან რამდენიმე სტრინგის შეტყუპება
+# #მაგალითად - temperature = "25" 
+# #print(temperature + "40")
+
+city = "london"
+print(city)
+
+mother = 37
+father = 40
+brother = 10
+mfb = mother + father + brother
+print(mfb / 3)
+
+num1 = 4
+num2 = 5
+
+print(num1 * num1)
+print(num2 * num2)
+
+print(num2 * num2 + num1 * num1)
+Num1 = 4
+Num2 = 6
+print(6 + 4)
+print(6 * 4)
+print(6 / 4)
+print(6 - 4)
+
+
+# კომენტარი - რაც არ უნდა დავწეროთ კომენტარში პროგრამა დააიგნორებს კომენტარი გამოიყენება როგორც ინსტრუქცია ადამიანისთვ
+
+
+
+points = 50
+points = 60
+points = 33
+print(points)
+
+#snake case - ვიყენებთ მაშინ როდესაც ერთზე მეტ სიტყვას ვიყენებთ პითონში ერთმანეთისგან გამოვყფთ _ ის საშუალებით
+#მაგალითად cat_name credit_score dog_name
+
+# name - nikolozi
+# print - Name
+# my age" - 25
+# friend_age _ 26
+# print(friend-age+my-age)
+# nUm2-11
+# nUm-17
+# print(nUm2+num)
+# print(1+4+5*8")
+# number1-5
+
+
+
+# name = nikolozi
+# print(name)
+# my_age = 25
+# friend_age = 26
+# print(my_age + my_age)
+# nUm2 = 11
+# nUm = 17
+# print(nUm2 + num)
+# print(1+4+5*8)
+# number1 = 5
+
+
+favorite_num = 7
+my_age = 12
+NUM3 = (my_age * favorite_num)
+print(NUM3)
+
+
+point = 2
+print(point * point / point + point - point)
+
+
+
+
+
---
+++ level 01/homework/lesson01.py
@@ -0,0 +1,4 @@
+name = "nikoloz"
+second_name = "tarxnishvili"
+old = str(12)
+print("მე ვარ " + name+" " + second_name+" " + old + " წლის")
