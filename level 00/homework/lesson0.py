69d9e1aadebea028dc17774c08a344f839619514
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2024-11-13 08:31:04 -0800

    GOA

--- level 00/homework/lesson0.py
+++ level 00/homework/lesson0.py
@@ -1,78 +1,45 @@
-print("nikoloztarxnishvili" * 100)
+from turtle import *
+color("red")
+begin_fill()
+forward(200)
+left(90)
+forward(150)
+right(270)
+forward(200)
+end_fill()
 
-# #concatination - კონკატინაცია არის ორი ან რამდენიმე სტრინგის შეტყუპება
-# #მაგალითად - temperature = "25" 
-# #print(temperature + "40")
+color("purple")
+begin_fill()
+right(135)
+forward(143)
+left(270)
+forward(143)
+right(135)
+forward(200)
+end_fill()
 
-city = "london"
-print(city)
-
-mother = 37
-father = 40
-brother = 10
-mfb = mother + father + brother
-print(mfb / 3)
-
-num1 = 4
-num2 = 5
-
-print(num1 * num1)
-print(num2 * num2)
-
-print(num2 * num2 + num1 * num1)
-Num1 = 4
-Num2 = 6
-print(6 + 4)
-print(6 * 4)
-print(6 / 4)
-print(6 - 4)
-
-
-# კომენტარი - რაც არ უნდა დავწეროთ კომენტარში პროგრამა დააიგნორებს კომენტარი გამოიყენება როგორც ინსტრუქცია ადამიანისთვ
-
-
-
-points = 50
-points = 60
-points = 33
-print(points)
-
-#snake case - ვიყენებთ მაშინ როდესაც ერთზე მეტ სიტყვას ვიყენებთ პითონში ერთმანეთისგან გამოვყფთ _ ის საშუალებით
-#მაგალითად cat_name credit_score dog_name
-
-# name - nikolozi
-# print - Name
-# my age" - 25
-# friend_age _ 26
-# print(friend-age+my-age)
-# nUm2-11
-# nUm-17
-# print(nUm2+num)
-# print(1+4+5*8")
-# number1-5
+color("red")
+color("red")
+right(270)
+forward(150)
+right(270)
+forward(75)
+begin_fill()
+color("yellow")
+left(90)
+forward(60)
+right(90)
+forward(50)
+right(90)
+forward(60)
+right(90)
+forward(50)
+end_fill()
 
 
 
-# name = nikolozi
-# print(name)
-# my_age = 25
-# friend_age = 26
-# print(my_age + my_age)
-# nUm2 = 11
-# nUm = 17
-# print(nUm2 + num)
-# print(1+4+5*8)
-# number1 = 5
+exitonclick()
 
-
-favorite_num = 7
-my_age = 12
-NUM3 = (my_age * favorite_num)
-print(NUM3)
-
-
-point = 2
-print(point * point / point + point - point)
 
 
 
---
+++ level 04/homework/lesson 04.py
@@ -0,0 +1,21 @@
+point = 50
+point = 51
+point = 13
+
+#snake_case - ვიყენებთ მაშინ როდესაც ერთზე მეტ სიტყვას ვიყენებთ პითონში ერთმანეთისგან გამოვყფთ _ ის საშუალებით
+#mad_dog
+#happy_human
+#clever_people
+
+
+
+
+
+my_faourite_num1 = 15
+my_age = 12
+num3 = my_faourite_num1 * my_age
+
+
+
+point = 5
+print(point * point + point / point - point)
