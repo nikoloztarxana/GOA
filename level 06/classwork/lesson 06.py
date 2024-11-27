b7b28d1a4f06ae5440b4fb55dbe03afde4bedecf
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2024-11-26 23:45:58 -0800

    goa

--- level 06/classwork/lesson 06.py
+++ level 06/classwork/lesson 06.py
@@ -1,4 +1,4 @@
-name = " nikolozi "
+name = " nikoloz "
 surname = " tarxnishvili "
 hello = " გამარჯობა "
 str1 = " შენ ხარ "
---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,15 @@
+input("enter your age")
+age = 13
+print(int(age))
+print(str(age))
+print(float(age))
+
+
+
+#int() ფუნქცია გვეხმარება რომ მინიშვნელობა გარდავქმნად ინტეჯერად
+#str() ფუნქცია გვეხმარება რომ მნიშვნელობა გარდავქმნად სტრინგად
+#float() ფუნქცია გვეხმარება რომ მინშვნელობა გარდავქმნად ათწილადად
+
+input("enter you age")
+age = 13
+print(int(age + 5))
