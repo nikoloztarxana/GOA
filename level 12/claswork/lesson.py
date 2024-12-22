9ac0bd300a93f8842b444585f19dec9dce957932
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2024-12-22 07:40:11 -0800

    GOA

---
+++ level 12/claswork/lesson.py
@@ -0,0 +1,20 @@
+n1 = 7 > 9 #false
+n2 = 11 > 10 #true
+n3 = 13 > 12 #true
+n4 = 13 + 12 > 13 #true
+n5 = 11 > 12 #false
+n6 = 11 > 10 #true
+n7 = 15 > 12#true
+n8 = 15 > 12 #true
+n9 = 12 > 12 #false
+n10 = 99 > 98 #true
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+print(n5)
+print(n6)
+print(n7)
+print(n8)
+print(n9)
+print(n10)
---
+++ level 12/homework/lesson.py
@@ -0,0 +1,22 @@
+a = 5 == 5#true
+b = 10 != 20#true
+c = 4 > 3 and 2 < 5#true
+d = 10 > 20 or 5 < 8#true
+e = not (7 > 3)#false
+f = "hello" == "world"#false
+g = 15 >= 10#true
+h = 8 <= 8#true
+i = 3 > 1 and 2 == 2 and 4 != 5#false
+j = not (10 < 5 or 2 == 2)#false
+
+
+print(5 == 5)
+print(10 != 20)
+print(4 > 3 and 2 < 5)
+print(10 > 20 or 5 < 8)
+print(not (7 > 3))
+print("hello" == "world")
+print(15 >= 10)
+print(8 <= 8)
+print(3 > 1 and 2 == 2 and 4 != 5)
+print(not (10 < 5 or 2 == 2))
