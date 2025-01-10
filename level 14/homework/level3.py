633d21d046fa5dad9eaeb3d2299c191f05c882c6
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2025-01-10 04:42:19 -0800

    GOA

---
+++ level 14/homework/level3.py
@@ -0,0 +1,12 @@
+#შეურიეთ შედარების და ლოგიკური ოპერატორები და გააკეთეთ ისევ 10 მაგალითი 
+
+
+height=float(input("enter your height"))
+age=int(input("enter your age"))
+"name"=input("enter your name")
+
+
+
+print(age>18)
+print(height>1.80)
+print("name"=="nikolozi")
