7aff9b8aab631043a5fba92debdea7592dc31d99
nikoloztarxana <nikoloztarxnishvili9@gmail.com>
2025-01-19 07:47:27 -0800

    GOA

---
+++ level 16/homework/level6.py
@@ -0,0 +1,13 @@
+#გააკეთეთ მინიმუმ 5 მაგალითი შედარების ოპერატორებზე და მინიმუმ 5მაგალითი ლოგიკურ ოპერატორებზე
+
+
+#logical
+print("true" or "false")
+print("false" not "false")
+print()
+print()
+print()
+#comparison
+
+
+
